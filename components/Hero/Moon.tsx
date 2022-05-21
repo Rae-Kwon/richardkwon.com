4b15f346a1001b52460a0a26c8df74157b001586
Richard Kwon <rkminjae@gmail.com>
2022-05-21 13:06:36 -0400

    fix sun and moon animations

--- components/Hero/Moon.tsx
+++ components/Hero/Moon.tsx
@@ -1,8 +1,20 @@
 import { motion } from "framer-motion";
 
 const variants = {
-  isDark: { translateX: 0, opacity: 1, ease: "easeOut" },
-  isLight: { translateY: -1000, opacity: 0, ease: "easeIn" },
+  isDark: {
+    translateY: 0,
+    opacity: 1,
+    ease: "easeIn",
+    transition: { type: "spring", duration: 5, stiffness: 50 },
+    originX: 0.5,
+  },
+  isLight: {
+    translateY: -1000,
+    opacity: 0,
+    ease: "easeIn",
+    transition: { type: "spring", duration: 5, stiffness: 50 },
+    originX: 0.5,
+  },
 };
 
 const Moon = ({ theme, setTheme }: any) => {
@@ -14,6 +26,7 @@ const Moon = ({ theme, setTheme }: any) => {
       variants={variants}
       initial={false}
       className="w-40 h-40 rounded-full bg-white shadow-xl shadow-white absolute"
+      role="button"
     />
   );
 };
--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -8,7 +8,7 @@ import Sun from "./Sun";
 const Hero = () => {
   const [mounted, setMounted] = useState(false);
 
-  const { theme, setTheme } = useTheme();
+  const { resolvedTheme, setTheme } = useTheme();
 
   useEffect(() => setMounted(true), []);
 
@@ -17,8 +17,8 @@ const Hero = () => {
   return (
     <header className="h-screen grid grid-rows-3 justify-center break-word">
       <div className="flex justify-center items-center">
-        <Sun theme={theme} setTheme={setTheme} />
-        <Moon theme={theme} setTheme={setTheme} />
+        <Sun theme={resolvedTheme} setTheme={setTheme} />
+        <Moon theme={resolvedTheme} setTheme={setTheme} />
       </div>
       <Intro />
       <ActionArrow />
