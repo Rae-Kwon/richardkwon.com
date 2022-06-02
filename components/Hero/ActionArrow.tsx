d26554841df353e9b0b1c5e98e305dcf25f9cc9a
Richard Kwon <rkminjae@gmail.com>
2022-06-02 15:24:06 -0400

    add link to action arrow

--- components/Hero/ActionArrow.tsx
+++ components/Hero/ActionArrow.tsx
@@ -1,27 +1,30 @@
 import { motion } from "framer-motion";
+import Link from "next/link";
 
 const ActionArrow = () => {
   return (
     <div className="flex justify-center items-end mb-20">
-      <div className="flex flex-col items-center">
-        <h3 className="hero-action-heading">See what I can do</h3>
-        <motion.svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          strokeWidth="2"
-          animate={{ translateY: [0, 5, 0, 5, 0] }}
-          transition={{ duration: 2, repeat: Infinity }}
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M19 9l-7 7-7-7"
-          />
-        </motion.svg>
-      </div>
+      <Link href="#works">
+        <div className="flex flex-col items-center cursor-pointer">
+          <h3 className="hero-action-heading">See what I can do</h3>
+          <motion.svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth="2"
+            animate={{ translateY: [0, 5, 0, 5, 0] }}
+            transition={{ duration: 2, repeat: Infinity }}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M19 9l-7 7-7-7"
+            />
+          </motion.svg>
+        </div>
+      </Link>
     </div>
   );
 };
