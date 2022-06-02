1afd007e7e7b1ee8e1cb3dbe88c1bba9c8ddce52
Richard Kwon <rkminjae@gmail.com>
2022-06-02 17:09:28 -0400

    add about me section (#8)
    add about me section (#8)
    
    What changed?
    ===============
    This commit adds an about me section.
    
    Why?
    ==============
    This commit allows users to see details about me.

---
+++ components/About/Description.tsx
@@ -0,0 +1,46 @@
+const Description = () => {
+  return (
+    <div className="flex flex-col gap-8 text-xl font-text">
+      <p>
+        I am a software developer looking to learn and grow with like minded
+        people.
+      </p>
+      <p>
+        I have a Bachelor of Arts majoring in History and Japanese, and
+        graduated from Enspiral Dev Academy focusing on Full Stack Development.
+      </p>
+      <p>
+        My interest in software development started in University during my
+        Japanese studies. I frequently had to use language apps for learning and
+        for use in live translating which got me curious in the underlying tech
+        that was being used. As I was studying I saw myself learning to code in
+        my spare time and found that the logic and creativity really aligned
+        with how I thought and it became a fun outlet during my university
+        studies.
+      </p>
+      <p>
+        After graduation I kept coming back to programming and wanted a more
+        focused course of study. I opted to attend Enspiral Dev Academy where I
+        was able to further my programming knowledge and enhance my
+        collaborative skills.
+      </p>
+      <p>
+        I have now moved to the US where I am interested in working with people
+        and companies that are at the forefront of innovation in technology. I
+        think my New Zealand upbringing and technical skills will be a huge
+        asset to you.
+      </p>
+      <p>
+        I currently have&nbsp;
+        <span className="underline underline-offset-4 decoration-red-400">
+          work authorization
+        </span>
+        &nbsp;with a pending&nbsp;
+        <span className="underline underline-offset-4 decoration-green-300">
+          permanent residency visa.
+        </span>
+      </p>
+    </div>
+  );
+};
+export default Description;
