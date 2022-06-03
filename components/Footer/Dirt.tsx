3a13b7e2e46b18d556df1c6b63a3ed9ce005f6f9
Richard Kwon <rkminjae@gmail.com>
2022-06-02 22:56:09 -0400

    add more information to contact section (#13)
    add more information to contact section (#13)
    
    What changed?
    =================
    This commit adds my email address and a made by Richard Kwon mark.
    
    Why?
    =================
    This commit allows users the alternative of sending an email with their email client of choice rather than the built contact form. It also allows users to see the site was created by me, Richard Kwon.

--- components/Footer/Dirt.tsx
+++ components/Footer/Dirt.tsx
@@ -1,9 +1,11 @@
 import Contact from "./Contact";
+import MadeBy from "./MadeBy";
 
 const Dirt = () => {
   return (
     <div className="py-20 bg-amber-900 dark:bg-midnight-dirt w-screen">
       <Contact />
+      <MadeBy />
     </div>
   );
 };
---
+++ components/Footer/MadeBy.tsx
@@ -0,0 +1,10 @@
+const MadeBy = () => {
+  return (
+    <div className="flex justify-center pb-6 mb-10">
+      <p className="text-white font-text">
+        Made with ❤️ and ☕️ by Richard Kwon
+      </p>
+    </div>
+  );
+};
+export default MadeBy;
