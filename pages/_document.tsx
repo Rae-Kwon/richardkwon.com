6bb1cbae919be992ff0ec7f976fe7fec5fa3f31f
Richard Kwon <rkminjae@gmail.com>
2022-06-10 22:19:51 -0400

    add analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,24 @@ import { Head, Html, Main, NextScript } from "next/document";
 const Document = () => {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
+      </Head>
       <body className="bg-sky-400 dark:bg-midnight-blue overflow-x-hidden">
         <Main />
         <NextScript />
